b1bf886e75b59f26b5efa344c568953194df12d5
徐月 <xuyue@richinfo.cn>
2021-06-17 11:20:57 +0800

    修改一下mongodb连接方式

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const viewsH = require('../views/index')
 const controller = require('./middleware/controller')
 
 const db = require('./db')
-db.connect(dbUrl, { useNewUrlParser: true })
+db.connect(dbUrl, {useNewUrlParser: true ,useUnifiedTopology: true})
 
 const {logger, loggerErr} = require("./logs");
 const {formatRequestLogText, formatResponseLogText} = require("./util");
